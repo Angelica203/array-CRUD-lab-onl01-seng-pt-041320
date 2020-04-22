86925b82b7ad0e04e471c0ca5bffc95824f05c99
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-22 03:43:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,15 @@
-def create_an_empty_array
+def create_an_empty_array 
+array=[]
   
 end
 
 def create_an_array
+array=["red", "white","blue","green"]
   
 end
 
 def add_element_to_end_of_array(array, element)
+  array << element
   
 end
 
